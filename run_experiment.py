6937036b283649a5bdd24e3350b79a78f67f527e
lukaslangeweile <150951654+lukaslangeweile@users.noreply.github.com>
2024-05-27 10:42:11 +0200

    new experiment data

--- run_experiment.py
+++ run_experiment.py
@@ -1,10 +1,8 @@
 import numerosity_judgement
 import spacial_unmasking
-sub_id = 2
+sub_id = 8
 spacial_unmasking.initialize_setup()
-spacial_unmasking.start_trial(sub_id=sub_id, masker_type="pinknoise", stim_type="syllable")
-input("Press Enter to continue Experiment...")
 spacial_unmasking.start_trial(sub_id=sub_id, masker_type="babble", stim_type="syllable")
 
 """numerosity_judgement.initialize_setup()
-numerosity_judgement.start_trial(sub_id, n_reps=20)"""
+numerosity_judgement.start_trial(sub_id, n_reps=50)"""
